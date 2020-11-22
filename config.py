2290f0084553b9112968486900bca38335fa80f8
uwamahororachel <racheluwamahoro45@gmail.com>
2020-11-22 18:07:26 +0200

    adding additional configurations

--- config.py
+++ config.py
@@ -13,3 +13,15 @@ class Config:
     # NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
    	# ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
 
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
