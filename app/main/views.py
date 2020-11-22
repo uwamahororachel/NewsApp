893096639b4b9d0203eb7bbdf9241641dbf8ed32
uwamahororachel <racheluwamahoro45@gmail.com>
2020-11-22 18:10:57 +0200

    creating page viewer

---
+++ app/main/views.py
@@ -0,0 +1,19 @@
+from flask import render_template, request, redirect, url_for
+from . import main
+from ..requests import get_sources, get_articles
+from ..models import Sources
+
+# views
+
+@main.route('/')
+def index():
+	'''
+	view root page function that returns the index the page and its data
+	'''
+	source = get_sources('business')
+	sports_sources = get_sources('sports')
+	technology_sources = get_sources('technology')
+	entertainment_sources = get_sources('entertainment')
+	title = "News Source website"
+
+	return render_template('index.html', title=title, business=source, sports=sports_sources, technology=technology_sources, entertainment=entertainment_sources)
