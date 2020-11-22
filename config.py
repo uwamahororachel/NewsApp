6aec4b66e6d77becfd67439e80a46eae2bfd71af
uwamahororachel <racheluwamahoro45@gmail.com>
2020-11-22 18:06:23 +0200

    Adding configurations for the api and manager file

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+
+    SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_API_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+   
+  
+    SOURCE_API_KEY = '1934d059c6cb45bda9c41b8990949223'
+    print(SOURCE_API_KEY)
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+   	# ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+
